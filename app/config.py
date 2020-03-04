8bb97f6ca20c1ccd759716b46afd3b01b29e7e3e
Ziyue Zhang <violet@ZiyuedeMBP.lan>
2020-03-03 20:09:35 -0500

    333333

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 # ami_id = 'ami-a076eec9'
 class Config(object):
-    ami_id = 'ami-080bb209625b6f74f'
+    ami_id = 'ami-016a0c6ce4f0d6854git'
     zone = 'us-east-1a'
     key_name = 'ece1779_a1'
     security_group = ['sg-02c66fa2f82dba589']
