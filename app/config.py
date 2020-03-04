e5bbd92d2030b75b6f0a378f051952059aa12314
Ziyue Zhang <violet@ZiyuedeMacBook-Pro.local>
2020-03-04 15:31:42 -0500

    111111

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 # ami_id = 'ami-a076eec9'
 class Config(object):
-    ami_id = 'ami-026e50a1269d82dd3'
+    ami_id = 'ami-0b2e635df12279cbb'
     zone = 'us-east-1a'
     key_name = 'ece1779_a1'
     security_group = ['sg-041902e4d97796241']
