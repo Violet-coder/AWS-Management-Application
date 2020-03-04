e88aefdea9c209021914f0058ffdcd0fa2c54561
Ziyue Zhang <violet@ZiyuedeMBP.lan>
2020-03-03 20:00:13 -0500

    2222

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 # ami_id = 'ami-a076eec9'
 class Config(object):
-    ami_id = 'ami-024573baca7a270c7'
+    ami_id = 'ami-080bb209625b6f74f'
     zone = 'us-east-1a'
     key_name = 'ece1779_a1'
     security_group = ['sg-02c66fa2f82dba589']
