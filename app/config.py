88d542bd3868039056e1ff9195a6cf8b129f4283
Ziyue Zhang <violet@ZiyuedeMBP.lan>
2020-03-03 20:24:16 -0500

    555555

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ class Config(object):
     ami_id = 'ami-016a0c6ce4f0d6854'
     zone = 'us-east-1a'
     key_name = 'ece1779_a1'
-    security_group = ['sg-02c66fa2f82dba589']
+    security_group = ['sg-041902e4d97796241']
     subnet_id = 'subnet-3d00e762'
     ec2_name = 'ece1779_a2_new'
